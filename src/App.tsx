329ea0f1c6b082e90b5f1796d6dc5a7684f96731
OLUJOHN2 <olumoyepelumi1@gmail.com>
2026-03-27 10:45:54 +0100

    Draggable cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
 import Loader from "./components/Loader";
 import ScrollIndicator from "./components/ScrollIndicator";
 
+import DraggableCard from "./components/DraggableCard";
+
 const App = () => {
   return (
     <div>
       {/* <ScrollIndicator /> */}
-
-      <Loader />
+      {/* <Loader /> */}
+      <div className="flex justify-center items-center h-[100vh] bg-[#f3f3f3]">
+        <DraggableCard
+          style={{
+            background: "linear-gradient(135deg, #f093fb 0%, #f5576c 100%)",
+          }}
+        >
+          Card 1
+        </DraggableCard>
+        <DraggableCard
+          style={{
+            background: "linear-gradient(135deg, #5ee7df 0%, #b490ca 100%)",
+          }}
+        >
+          Card 2
+        </DraggableCard>
+        <DraggableCard
+          style={{
+            background: "linear-gradient(135deg, #ff9a9e 0%, #fecfef 99%)",
+          }}
+        >
+          Card 3
+        </DraggableCard>
+      </div>
     </div>
   );
 };
